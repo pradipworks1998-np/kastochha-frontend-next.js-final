8fe6c8cee6f4ad71b4871f3570701f977897c485
Pradip <pradipworks1998@gmail.com>
2026-01-14 15:50:28 +0545

    TBT fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add this section below:
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 module.exports = nextConfig;
